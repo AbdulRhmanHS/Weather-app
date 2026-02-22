25b128a64424edc6ee1e600f143d7084c81c2f3b
AbdulRhman <bodahassan12@gmail.com>
2026-02-22 15:06:55 +0200

    Make functions to get necessary data

---
+++ src/core/data.js
@@ -0,0 +1,65 @@
+import { format, parse, differenceInCalendarDays } from 'date-fns';
+
+function getRemainingDays(date) {
+  const today = new Date();
+  const due = new Date(date);
+
+  const remaining = differenceInCalendarDays(due, today);
+  return remaining;
+}
+
+// 1. Getting data for 15 days
+export function getDaysData(weatherData) {
+  let results = [];
+  for (let i = 0; i < 15; i++) {
+    let temp = `Temperature: ${weatherData.days[i].tempmin} / ${weatherData.days[i].tempmax}`;
+
+    let remaining = getRemainingDays(weatherData.days[i].datetime);
+    let due = new Date(weatherData.days[i].datetime);
+    let day = 'Today';
+    if (remaining === 1) day = 'Tomorrow';
+    if (remaining > 1 || remaining < 0) day = format(due, 'd MMM');
+
+    results.push({
+      condition: weatherData.days[i].conditions,
+      temperature: temp,
+      time: day,
+    });
+  }
+  return results;
+}
+
+// 2. Getting data of the day for up to 23 hours
+export function getHourlyData(weatherData) {
+  const now = new Date();
+  let hours = now.getHours();
+  let results = [];
+  for (let i = hours; i < hours + 24; i++) {
+    const dayIndex = i < 24 ? 0 : 1;
+    const hourIndex = i % 24;
+    const hourData = weatherData.days[dayIndex].hours[hourIndex];
+
+    let hourObj = parse(hourData.datetime, 'HH:mm:ss', new Date());
+    let hours = format(hourObj, 'h a');
+
+    results.push({
+      condition: hourData.conditions,
+      temperature: hourData.temp,
+      time: hours,
+    });
+  }
+  return results;
+}
+
+export function getTodayData(weatherData) {
+  let results = [];
+
+  results.push({
+    temperature: weatherData.days[0].temp,
+    precipitation: weatherData.days[0].precip,
+    windSpeed: weatherData.days[0].windspeed,
+    humidity: weatherData.days[0].humidity,
+  });
+
+  return results;
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('Weather-app');
+import weatherData from './example.json';
+import { getDaysData, getHourlyData, getTodayData } from './core/data';
+
+// async function getWeatherData() {
+//   try {
+//     const response = await fetch('url');
+
+//     if (!response.ok) throw new Error('File not found');
+
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.error('Oops! Something went wrong:', error);
+//   }
+// }
+
+console.log(weatherData);
+console.log(getDaysData(weatherData));
+console.log(getHourlyData(weatherData));
+console.log(getTodayData(weatherData));
